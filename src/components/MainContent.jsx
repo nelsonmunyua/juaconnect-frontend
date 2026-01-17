5325f633ca97c634233334aa04bf732cdaf9d6cd
Newton Ongeri <ongerinewton10@gmail.com>
2026-01-17 10:12:27 +0300

    deleted sign up, sign in and landing page and modified some files

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import TopBar from './TopBar';
 import UpcomingBookings from './UpcomingBookings';
 import Payments from './Payments';
+import BookingsPage from './BookingsPage';
+import PaymentsPage from './PaymentsPage';
+import NotificationsPage from './NotificationsPage';
+import ProfilePage from './ProfilePage';
 
-const MainContent = ({ searchQuery, setSearchQuery }) => {
+const MainContent = ({ activeNav, searchQuery, setSearchQuery }) => {
   return (
     <main className="flex-1 overflow-y-auto">
       <TopBar searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       
       <div className="p-6 space-y-8">
-        <UpcomingBookings />
-        <Payments />
+        {activeNav === 'dashboard' && (
+          <>
+            <UpcomingBookings />
+            <Payments />
+          </>
+        )}
+        {activeNav === 'bookings' && <BookingsPage />}
+        {activeNav === 'payments' && <PaymentsPage />}
+        {activeNav === 'notifications' && <NotificationsPage />}
+        {activeNav === 'profile' && <ProfilePage />}
       </div>
     </main>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,11 @@
 // src/main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/pages/Home.jsx";
-import SignIn from "./components/auth/SignIn.jsx";
-import SignUp from "./components/auth/SignUp.jsx";
-
-
-
-
-const routes = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-    path: '/signin',
-    element: <SignIn />
-  },
-  {
-    path: '/signup',
-    element: <SignUp />
-  },
-  
-]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={routes} />
+    <App />
   </StrictMode>
 );
